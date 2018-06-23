bee4e004a0b556fcf6d85a80f9f9df2fefe28986
ThePiGuy0 <thepiguy0@gmail.com>
2018-06-23 19:40:16 +0100

    lineage.mk: Revert to last official signature Enables CTS to pass
    Change-Id: I28dff88d02203bd6ae764526c194e25abb237739

--- lineage.mk
+++ lineage.mk
@@ -15,6 +15,6 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
     PRODUCT_NAME="g4__tmo_us" \
-    PRIVATE_BUILD_DESC="p1__tmo_us-user 8.0 NRD90U 1717709471d0f release-keys"
+    PRIVATE_BUILD_DESC="p1__tmo_us-user 6.0 MRA58K 152940055675e release-keys"
 
-BUILD_FINGERPRINT := "lge/p1__tmo_us/p1:8.1.0/OPM4.171019.016.A1/4720811:user/release-keys"
+BUILD_FINGERPRINT := "lge/p1__tmo_us/p1:6.0/MRA58K/152940055675e:user/release-keys"
