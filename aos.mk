d16cc9ccb8571808f356fc11bac38336a925355d
aoleary <seanm187@gmail.com>
2019-01-03 21:35:05 +0100

    Atomize and simplify the H811 device tree

--- aos.mk
+++ aos.mk
@@ -1,12 +1,17 @@
-$(call inherit-product, device/lge/h811/full_h811.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from h815 device
+$(call inherit-product, device/lge/h811/device.mk)
 
 # Inherit some common AOSCP stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aos/config/common.mk)
 
 # Overlays (inherit after vendor/lineage to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := lineage_h811
+PRODUCT_NAME := aos_h811
 PRODUCT_DEVICE := h811
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H811
